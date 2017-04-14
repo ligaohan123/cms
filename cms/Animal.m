5b7fb8f4751438e3470d802c6fbad9d826fb0de6
李高晗 <522651493@qq.com>
2017-04-14 22:43:02 +0800

    animal class

---
+++ cms/Animal.m
@@ -0,0 +1,18 @@
+//
+//  Animal.m
+//  cms
+//
+//  Created by 李高晗 on 2017/4/14.
+//  Copyright © 2017年 李高晗. All rights reserved.
+//
+
+#import "Animal.h"
+
+@implementation Animal
+
+- (void)eat
+{
+    NSLog(@"animal eat");
+}
+
+@end
