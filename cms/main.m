9c0bc3a7a42cba1ca00a7cd3794c27a95044bba3
李高晗 <522651493@qq.com>
2017-04-14 22:34:30 +0800

    Initial Commit

---
+++ cms/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  cms
+//
+//  Created by 李高晗 on 2017/4/14.
+//  Copyright © 2017年 李高晗. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
